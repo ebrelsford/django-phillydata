a5fa21f46c7a24897650734b14a0f350e32f0ae4
Eric Brelsford <ebrelsford@gmail.com>
2014-10-06 22:15:22 -0400

    BaseDataSourceAdmin

--- phillydata/admin.py
+++ phillydata/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 
-from external_data_sync.admin import DataSourceAdmin
+from external_data_sync.admin import BaseDataSourceAdmin
 
 from .models import PhillyDataSource
 
 
-class PhillyDataSourceAdmin(DataSourceAdmin):
+class PhillyDataSourceAdmin(BaseDataSourceAdmin):
     pass
 
 admin.site.register(PhillyDataSource, PhillyDataSourceAdmin)
