ce58220af15c0421a36feac1ab01e0368bf49219
Eric Brelsford <ebrelsford@gmail.com>
2014-10-15 19:45:20 -0400

    Use the new OPA API
    See: http://phlapi.com/opaapi.html

--- phillydata/opa/adapter.py
+++ phillydata/opa/adapter.py
@@ -4,8 +4,9 @@ Turns data from the OPA API into the relevant models.
 Kind of a proxy/adapter for OPA data.
 
 """
-from datetime import datetime
+import datetime
 import logging
+import re
 
 import reversion
 
@@ -19,51 +20,76 @@ logger = logging.getLogger(__name__)
 
 SALE_DATE_FORMAT = '%m/%d/%Y'
 
+def json_date_as_datetime(json_date):
+    """Parse date in .NET JSON format, eg /Date(1154970000000+0700)/"""
+    # Get rid of annoying JSON date cruft
+    json_date = re.sub('\/Date\(', '', json_date)
+    json_date = re.sub('\)\/', '', json_date)
+
+    # Watch for initial - sign
+    negative = False
+    if json_date[0] == '-':
+        json_date = json_date[1:]
+        negative = True
+
+    # Grab timestamp, offset
+    try:
+        timestamp, offset = json_date.split('+')
+        offset = int(offset)
+    except ValueError:
+        try:
+            timestamp, offset = json_date.split('-')
+            offset = -int(offset)
+        except ValueError:
+            timestamp = json_date
+            offset = None
+
+    ms = int(timestamp) * 1000
+    if negative:
+        ms = -ms
+    if offset:
+        # Convert offset to ms
+        ms += offset * 60 * 60 * 10000
+
+    # Return epoch + timestamp
+    return datetime.datetime(1970, 1, 1) + datetime.timedelta(microseconds=ms)
+
+
 def billing_account_kwargs(data):
     return {
-        'external_id': data['account_information']['account_number'],
+        'external_id': data['account_number'],
     }
 
 
-def billing_account_defaults(data, defaults={}):
-    acct_det = data['account_details']
-    defaults.update(get_address(data))
+def get_assessment(valuation_history):
+    if not valuation_history:
+        return None
+    most_recent = max(valuation_history, key=lambda entry: entry['certification_year'])
+    return most_recent['market_value']
+
+
+def billing_account_defaults(characteristics=None, sales_information=None,
+                             valuation_history=None, defaults={}, **kwargs):
     defaults.update({
-        'sale_date': datetime.strptime(acct_det['sale_date'], SALE_DATE_FORMAT),
-        'land_area': float(acct_det['land_area'].split()[0]),
+        'sale_date': json_date_as_datetime(sales_information['sales_date']),
+        'land_area': characteristics['land_area'],
 
         # TODO NB: if 0, could indicate vacancy
-        'improvement_area': float(acct_det['improvement_area'].split()[0]),
+        'improvement_area': characteristics['improvement_area'],
 
         # TODO NB: if starts with 'VAC LAND', could indicate vacancy
-        'improvement_description': acct_det['improvement_desc'],
+        'improvement_description': characteristics['improvement_description'],
 
         # TODO NB: if 0, could indicate publicly owned?
-        'assessment': str(acct_det['assessment']).translate(None, '$,'),
+        'assessment': get_assessment(valuation_history),
     })
     return defaults
 
 
-def get_address(data):
-    addr = data['account_information']['mailing_address']
-    address_details = {
-        'property_address': data['account_information']['address'],
-        'mailing_name': addr['street'][0],
-        'mailing_address': addr['street'][1],
-        'mailing_city': addr['city'],
-        'mailing_state_province': addr['state'],
-        'mailing_postal_code': addr['zip'],
-    }
-    # html_unescape everything
-    return dict([(k, html_unescape(v)) for k, v in address_details.items()])
-
-
 @reversion.create_revision()
-def get_or_create_account_owner(data):
-    account = data['account_information']
-
+def get_or_create_account_owner(ownership=None, **kwargs):
     # sorting owner names to keep internally consistent
-    owner_name = ', '.join(sorted(account['owners']))
+    owner_name = ', '.join(sorted(ownership['owners']))
     owner_name = html_unescape(owner_name)
 
     account_owner, created = AccountOwner.objects.get_or_create(
@@ -75,8 +101,8 @@ def get_or_create_account_owner(data):
 
 @reversion.create_revision()
 def get_or_create_billing_account(data, account_owner):
-    defaults = billing_account_defaults(data,
-                                        {'account_owner': account_owner,})
+    defaults = billing_account_defaults(defaults={'account_owner': account_owner,},
+                                        **data)
     billing_account, created = BillingAccount.objects.get_or_create(
         defaults=defaults, **billing_account_kwargs(data))
     if not created:
@@ -99,5 +125,5 @@ def find_opa_details(address, brt_account=None):
     if not data:
         raise Exception('Could not find OPA details for "%s"' % address)
 
-    account_owner = get_or_create_account_owner(data)
+    account_owner = get_or_create_account_owner(**data)
     return get_or_create_billing_account(data, account_owner)
--- phillydata/opa/api.py
+++ phillydata/opa/api.py
@@ -1,30 +1,33 @@
 import json
 import logging
-from urllib import quote_plus
-from urllib2 import urlopen, URLError
+from urllib import urlencode
+from urllib2 import quote, urlopen, URLError
 
 logger = logging.getLogger(__name__)
 
 
-BASE_URL = 'http://api.phillyaddress.com/'
+BASE_URL = 'http://api.phila.gov/opa/v1.0/'
 ADDRESS_ENDPOINT = 'address/'
 ACCOUNT_ENDPOINT = 'account/'
 
+params = {
+    'format': 'json',
+}
 
 def get_address_data(address):
     try:
-        url = BASE_URL + ADDRESS_ENDPOINT + quote_plus(address)
-        data = json.load(urlopen(url, None, 30))
+        url = BASE_URL + ADDRESS_ENDPOINT + quote(address) + '/?' + urlencode(params)
+        data = json.load(urlopen(url, None, 30))['data']
         return data['property']
     except KeyError:
-        logger.debug(('Could not find property in response for %s. Trying by '
-                      'account number') % address)
+        logger.debug(('Could not find unique property in response for %s. '
+                      'Trying by account number') % address)
 
         # Try to find a matching property in the response's properties
         if 'properties' in data:
             for prop in data['properties']:
-                if prop['address'].lower() == address.lower():
-                    return get_account_data(prop['account_number'])
+                if prop['full_address'].lower() == address.lower():
+                    return prop
 
         logger.debug('Could not find property by account number, either.')
         return None
@@ -35,8 +38,8 @@ def get_address_data(address):
 
 def get_account_data(account):
     try:
-        url = BASE_URL + ACCOUNT_ENDPOINT + account
-        data = json.load(urlopen(url))
+        url = BASE_URL + ACCOUNT_ENDPOINT + account + '/?' + urlencode(params)
+        data = json.load(urlopen(url))['data']
         return data['property']
     except Exception:
         logger.exception('Exception while getting OPA data for account %s'
