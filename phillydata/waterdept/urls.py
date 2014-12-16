e23559b39c27d95e99274ffb2e30035f7c83c36f
Eric Brelsford <ebrelsford@gmail.com>
2014-12-15 21:24:03 -0500

    GeoJSON for individual water department parcels

--- phillydata/waterdept/urls.py
+++ phillydata/waterdept/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, url
 
-from .views import GeoJSONParcelListView
+from .views import GeoJSONParcelDetailView, GeoJSONParcelListView
 
 
 urlpatterns = patterns('',
+    url(r'^(?P<pk>\d+)/geojson/', GeoJSONParcelDetailView.as_view(),
+        name='waterparcel_detail_geojson'),
     url(r'^geojson/', GeoJSONParcelListView.as_view(), name='waterparcel_geojson'),
 )
--- phillydata/waterdept/views.py
+++ phillydata/waterdept/views.py
@@ -3,12 +3,11 @@ import geojson
 
 from django.contrib.gis.geos import Polygon
 
-from inplace.views import GeoJSONListView
+from inplace.views import GeoJSONDetailView, GeoJSONListView
 from .models import WaterParcel
 
 
-class GeoJSONParcelListView(GeoJSONListView):
-    model = WaterParcel
+class GeoJSONParcelMixin(object):
 
     def get_feature(self, parcel):
         return geojson.Feature(
@@ -21,6 +20,18 @@ class GeoJSONParcelListView(GeoJSONListView):
         return {
             'address': parcel.address,
         }
+
+
+class GeoJSONParcelDetailView(GeoJSONParcelMixin, GeoJSONDetailView):
+    model = WaterParcel
+
+    def get_queryset(self):
+        return WaterParcel.objects.filter(geometry__isnull=False) \
+                .geojson(precision=6)
+
+
+class GeoJSONParcelListView(GeoJSONParcelMixin, GeoJSONListView):
+    model = WaterParcel
 
     def get_queryset(self):
         try:
